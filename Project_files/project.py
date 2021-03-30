ee7510ee8a4e9ad360f38e988a4a2069b63378ad
fedenemi <39474959+fedenemi@users.noreply.github.com>
2021-03-30 09:53:36 -0300

    add dummy variables, task 3

--- Project_files/project.py
+++ Project_files/project.py
@@ -5,6 +5,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
 
+def dummy_creation(df, dummy_categories):
+    for i in dummy_categories:
+        df_dummy = pd.get_dummies(df[i])
+        df = pd.concat([df,df_dummy],axis=1)
+        df = df.drop(i, axis=1)
+    return(df)
+
 df = pd.read_csv('/path/to/file/pokemon.csv')
 
 df = df[['isLegendary','Generation', 'Type_1', 'Type_2', 'HP', 'Attack', 'Defense', 'Sp_Atk', 'Sp_Def', 'Speed','Color','Egg_Group_1','Height_m','Weight_kg','Body_Style']]
+
+#df['isLegendary'] = df['isLegendary'].astype(int)
+df = dummy_creation(df, ['isLegendary'])
+df = dummy_creation(df, ['Egg_Group_1', 'Body_Style', 'Color','Type_1', 'Type_2'])
+
