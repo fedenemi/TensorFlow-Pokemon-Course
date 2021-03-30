4098f4be5498ef17c2176d7b3ccca620078adabf
fedenemi <39474959+fedenemi@users.noreply.github.com>
2021-03-30 09:33:41 -0300

    add second task

--- Project_files/project.py
+++ Project_files/project.py
@@ -4,3 +4,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
+
+df = pd.read_csv('/path/to/file/pokemon.csv')
+
+df = df[['isLegendary','Generation', 'Type_1', 'Type_2', 'HP', 'Attack', 'Defense', 'Sp_Atk', 'Sp_Def', 'Speed','Color','Egg_Group_1','Height_m','Weight_kg','Body_Style']]
