c36a5d516e09abcdaf6b5d13ed62566d688d370a
fedenemi <39474959+fedenemi@users.noreply.github.com>
2021-04-02 15:33:43 -0300

    Add model

--- Project_files/project.py
+++ Project_files/project.py
@@ -12,14 +12,6 @@ def dummy_creation(df, dummy_categories):
         df = df.drop(i, axis=1)
     return(df)
 
-df = pd.read_csv(r'C:\Users\Fede\Documents\GitHub\TensorFlow-Pokemon-Course\Project_files\pokemonez.csv')
-
-df = df[['isLegendary','Generation', 'Type_1', 'Type_2', 'HP', 'Attack', 'Defense', 'Sp_Atk', 'Sp_Def', 'Speed','Color','Egg_Group_1','Height_m','Weight_kg','Body_Style']]
-
-#df['isLegendary'] = df['isLegendary'].astype(int)
-df = dummy_creation(df, ['isLegendary'])
-df = dummy_creation(df, ['Egg_Group_1', 'Body_Style', 'Color','Type_1', 'Type_2'])
-
 def train_test_splitter(DataFrame, column):
     df_train = DataFrame.loc[df[column] != 1]
     df_test = DataFrame.loc[df[column] == 1]
@@ -29,8 +21,6 @@ def train_test_splitter(DataFrame, column):
 
     return(df_train, df_test)
 
-df_train, df_test = train_test_splitter(df, 'Generation')
-
 def label_delineator(df_train, df_test, label):
     
     train_data = df_train.drop(label, axis=1).values
@@ -39,5 +29,42 @@ def label_delineator(df_train, df_test, label):
     test_labels = df_test[label].values
     return(train_data, train_labels, test_data, test_labels)
 
+def data_normalizer(train_data, test_data):
+
+    train_data = preprocessing.MinMaxScaler().fit_transform(train_data)
+
+    test_data = preprocessing.MinMaxScaler().fit_transform(test_data)
+
+    return(train_data, test_data)    
+
+#upload csv and put the part that we want in the dataframe
+
+df = pd.read_csv(r'C:\Users\Fede\Documents\GitHub\TensorFlow-Pokemon-Course\Project_files\pokemonez.csv')
+
+df = df[['isLegendary','Generation', 'Type_1', 'Type_2', 'HP', 'Attack', 'Defense', 'Sp_Atk', 'Sp_Def', 'Speed','Color','Egg_Group_1','Height_m','Weight_kg','Body_Style']]
+
+#transform it to a useful data type
+
+df = dummy_creation(df, ['isLegendary'])
+df = dummy_creation(df, ['Egg_Group_1', 'Body_Style', 'Color','Type_1', 'Type_2'])
+
+#split the data
+
+df_train, df_test = train_test_splitter(df, 'Generation')
+
 train_data, train_labels, test_data, test_labels = label_delineator(df_train, df_test, 'isLegendary')    
-print(df)
+
+
+
+
+
+#Normalize and model data
+
+train_data, test_data = data_normalizer(train_data, test_data)  
+
+length = train_data.shape[1]
+model = keras.Sequential()
+
+model.add(keras.layers.Dense(500, activation='relu', input_shape=[length]))
+
+model.add(keras.layers.Dense(2, activation='softmax')) 
