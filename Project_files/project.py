586c1c8881c6699aea7f3ab840e932b3244a4507
fedenemi <39474959+fedenemi@users.noreply.github.com>
2021-04-02 13:15:34 -0300

    Add new functions

--- Project_files/project.py
+++ Project_files/project.py
@@ -12,7 +12,7 @@ def dummy_creation(df, dummy_categories):
         df = df.drop(i, axis=1)
     return(df)
 
-df = pd.read_csv('/path/to/file/pokemon.csv')
+df = pd.read_csv(r'C:\Users\Fede\Documents\GitHub\TensorFlow-Pokemon-Course\Project_files\pokemonez.csv')
 
 df = df[['isLegendary','Generation', 'Type_1', 'Type_2', 'HP', 'Attack', 'Defense', 'Sp_Atk', 'Sp_Def', 'Speed','Color','Egg_Group_1','Height_m','Weight_kg','Body_Style']]
 
@@ -20,3 +20,24 @@ df = df[['isLegendary','Generation', 'Type_1', 'Type_2', 'HP', 'Attack', 'Defens
 df = dummy_creation(df, ['isLegendary'])
 df = dummy_creation(df, ['Egg_Group_1', 'Body_Style', 'Color','Type_1', 'Type_2'])
 
+def train_test_splitter(DataFrame, column):
+    df_train = DataFrame.loc[df[column] != 1]
+    df_test = DataFrame.loc[df[column] == 1]
+
+    df_train = df_train.drop(column, axis=1)
+    df_test = df_test.drop(column, axis=1)
+
+    return(df_train, df_test)
+
+df_train, df_test = train_test_splitter(df, 'Generation')
+
+def label_delineator(df_train, df_test, label):
+    
+    train_data = df_train.drop(label, axis=1).values
+    train_labels = df_train[label].values
+    test_data = df_test.drop(label,axis=1).values
+    test_labels = df_test[label].values
+    return(train_data, train_labels, test_data, test_labels)
+
+train_data, train_labels, test_data, test_labels = label_delineator(df_train, df_test, 'isLegendary')    
+print(df)
